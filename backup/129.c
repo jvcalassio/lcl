f7fed30c4f2ca4125da9046aae92370d75f40357
João Coutinho <39359575+joaocout@users.noreply.github.com>
2018-06-07 13:42:35 -0300

    Add files via upload

--- backup/129.c
+++ backup/129.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 
-void LerVetor (long long vet[], long long n){
-	long long i;
-	for(i=0; i<n; i++){
-		scanf("%lld", &vet[i]);
+long long int posl (long long int vet[], long long int n, long long int x){
+	long long int l=0, r=n-1;
+	while(l<=r){
+		long long int mid = (l+r)/2;
+		if(vet[mid]>x)
+			r=mid-1;
+		else if(vet[mid]<x)
+			l=mid+1;
+		else if(vet[mid]==x){
+			if(mid==0 || vet[mid-1]!=x)
+				return mid;
+			else if(vet[mid-1]==x)
+				r=mid-1;
+		}
 	}
+	return 0;
 }
 
-long long Qtdx(long long vet[], long long n, long long x){
-	long long l=0, r=n-1, m=0, aux=1;
-	while(aux=1){
-		m=(l+r)/2;
-		if(vet[m]<x){
-			l=m+1;
+long long int posr(long long int vet[], long long int n, long long int x){
+	long long int l=0, r=n-1;
+	while(l<=r){
+		long long int mid = (l+r)/2;
+		if(vet[mid]>x)
+			r=mid-1;
+		else if(vet[mid]<x)
+			l=mid+1;
+		else if(vet[mid]==x){
+			if(mid==n-1 || vet[mid+1]!=x)
+				return mid+1;
+			else if(vet[mid+1]==x)
+				l=mid+1;
 		}
-		else if(vet[m]>x){
-			r=m-1;
-		}
-		else if(vet[m]==x){
-			if(vet[m]==vet[m-1]){
-				l=(l+m)/2;
-			}
-			if(vet[m]==vet[m+1]){
-				r=(r+m)/2;
-			}
-		}
-
 	}
-return r-l+1;
+	return 0;
 }
 
-int main () {
-	long long n, q, x, i;
+int main (){
+	long long int n, q;
 	scanf("%lld %lld", &n, &q);
-	long long a[n];
-	LerVetor(a, n);
+	long long int vet[n], i;
+	for(i=0; i<n; i++)
+		scanf("%lld", &vet[i]);
+	long long int x;
 	for(i=0; i<q; i++){
 		scanf("%lld", &x);
-		printf("%lld\n", Qtdx(a, n, x));
+		printf("%lld\n", posr(vet, n, x) - posl(vet, n, x));
 	}
 return 0;
 }
